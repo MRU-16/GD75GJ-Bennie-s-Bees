c758babc88b1a45f3a52a1f5cddfe528625f50a8
Bennie <benjaminkhadem11@gmail.com>
2025-04-21 16:18:34 -0700

    pushed some bad code

--- Assets/FRED Is On The Way/Bennie Scripts/Player Movement.cs
+++ Assets/FRED Is On The Way/Bennie Scripts/Player Movement.cs
@@ -5,19 +5,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float moveSpeed;
+    public float sensitivity;
     private Vector2 _moveDirection;
+    private Vector2 _lookDirection;
 
     public InputActionReference move;
+    public InputActionReference look;
+    public GameObject head;
 
     void Start()
     {
 
     }
 
-    
+
     void Update()
     {
         _moveDirection = move.action.ReadValue<Vector2>();
+        _lookDirection = look.action.ReadValue<Vector2>();
+        transform.rotation = head.transform.rotation;
+
 
     }
 
@@ -25,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector3(_moveDirection.x * moveSpeed, 0 ,_moveDirection.y * moveSpeed);
+        
     }
 
 
