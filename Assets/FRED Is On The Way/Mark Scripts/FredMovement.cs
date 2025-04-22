3690cce720c1f6ff48f11c3b8282e5ba5f3a382e
Markus Rivera <markus.riv16@gmail.com>
2025-04-21 18:29:35 -0700

    refactoring ai (WiP)

--- Assets/FRED Is On The Way/Mark Scripts/FredMovement.cs
+++ Assets/FRED Is On The Way/Mark Scripts/FredMovement.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 
 public class FredMovement : MonoBehaviour
 {
-    [SerializeField] public Transform tragetObj;
+    [SerializeField] public Transform target;
     [SerializeField] public int fredSpeed;
     [SerializeField] public int objectsCollected;
     [SerializeField] public int speedObject1;
+    [SerializeField] public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(this.transform.position, tragetObj.position, fredSpeed * Time.deltaTime);
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, fredSpeed * Time.deltaTime);
+        if (target.position != pos) 
+        {
+             pos = Vector3.MoveTowards(transform.position, target.position, fredSpeed * Time.deltaTime);
+        }
+        
+        rb.MovePosition(pos);
+        transform.LookAt(target);
     }
 
 }
