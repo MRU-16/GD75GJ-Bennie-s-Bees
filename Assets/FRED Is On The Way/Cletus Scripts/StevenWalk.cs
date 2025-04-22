c647870dabcb35620bebee7b8fc566cf292e90e4
Brett (Cletus) Hendrix <Gunsmith202@gmail.com>
2025-04-22 16:43:55 -0700

    Fixed Script Errors
    -Fixed Script Errors in steven walking script

--- Assets/FRED Is On The Way/Cletus Scripts/StevenWalk.cs
+++ Assets/FRED Is On The Way/Cletus Scripts/StevenWalk.cs
@@ -2,49 +2,29 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class StevenWalk : FredMovement
+public class StevenWalk : MonoBehaviour
 {
     [SerializeField] private Targets _target;
     [field: SerializeField] public FredEyes Vision { get; private set; }
     [SerializeField] private float _waypointReachedDistance = 20f;
     [SerializeField] private float _patrolSpeedMultiplier = 5f;
-    private Rigidbody _rigidbody;
-    [SerializeField] private float killDistance = 10f;
-    [SerializeField] private GameObject player;
-    [SerializeField] private BoxCollider _collider;
+    
 
     private NavMeshAgent enemy;
 
     [SerializeField] private string _currentStateName;
 
-    // useful properties
-    public bool IsTargetValid => _target != null && _target.IsTargetable;
-    public bool IsTargetVisible => Vision.TestVisibility(_target.transform.position);
-    public float TargetDistance => Vector3.Distance(_target.transform.position, transform.position);
-    public Vector3 TargetPosition => _target.transform.position;
-
     private IEnumerator _currentState;
     [SerializeField] private PatrolPoints[] _waypoints;
-    [SerializeField] private float attackDistance;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     private IEnumerator WanderState()
     {
         PatrolPoints patrolpoint = null;
         enemy.speed = _patrolSpeedMultiplier;
 
         // wander until valid target is spotted, then attack
-        while (!IsTargetValid)
+        while (true)
         {
 
             // find waypoint to move to
