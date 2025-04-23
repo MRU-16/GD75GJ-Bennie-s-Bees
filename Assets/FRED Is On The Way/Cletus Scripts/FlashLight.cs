04237643f69d17edf25c241f26cadf09e37f1cae
Brett (Cletus) Hendrix <Gunsmith202@gmail.com>
2025-04-23 09:59:21 -0700

    Added Flashlight hud
    -Added Flashlight hud
    -Added Flashlight recharge

--- Assets/FRED Is On The Way/Cletus Scripts/FlashLight.cs
+++ Assets/FRED Is On The Way/Cletus Scripts/FlashLight.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlashLight : MonoBehaviour
 {
     [SerializeField] private GameObject ON;
     [SerializeField] private GameObject OFF;
     [SerializeField] private bool isON;
+
+    [SerializeField] private Image FlashLightBar;
+    [SerializeField] private float LightTime, MaxLightTime;
+    [SerializeField] private float LightCost;
+    [SerializeField] private float RechargeTime;
+
     void Start()
     {
         ON.SetActive(false);
@@ -15,19 +22,41 @@ public class FlashLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.F))
+        if (LightTime <= MaxLightTime && (isON == false))
         {
+            LightTime += RechargeTime * Time.deltaTime;
+            FlashLightBar.fillAmount = LightTime / MaxLightTime;
+        }
+        if (isON == true)
+        {
+            LightTime -= LightCost * Time.deltaTime;
+            if (LightTime <= 0)
+            {
+                LightTime = 0;
+                OFF.SetActive(true);
+                ON.SetActive(false);
+                isON = false;
+            }
+            FlashLightBar.fillAmount = LightTime / MaxLightTime;
+
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.F) && (LightTime >= 0))
+        {
+            isON = !isON;
             if (isON)
             {
                 ON.SetActive(true);
                 OFF.SetActive(false);
+
+
             }
             if (!isON)
             {
                 ON.SetActive(false);
                 OFF.SetActive(true);
             }
-            isON = !isON;
         }
         
     }
